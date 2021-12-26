343a550e8a04763b9b101e402a6e906145219135
Scott Hardin <me@hardinscott.dev>
2021-12-26 18:28:37 -0500

    Initial Commit

---
+++ memorize/memorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  memorizeApp.swift
+//  memorize
+//
+//  Created by Scott Hardin on 12/26/21.
+//
+
+import SwiftUI
+
+@main
+struct memorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
