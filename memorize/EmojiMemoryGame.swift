2b16ec313bd95cfaa55a7808e90fa9c16a31047c
Scott Hardin <me@hardinscott.dev>
2022-03-12 17:47:28 -0500

    added themes

--- memorize/EmojiMemoryGame.swift
+++ memorize/EmojiMemoryGame.swift
@@ -5,14 +5,37 @@
 import SwiftUI
 
 class EmojiMemoryGame: ObservableObject{
-    static var emojis = ["🥶", "😈", "👺", "💩", "👾", "🤥", "😮‍💨", "🤮", "🤯", "😱", "😨", "🤩","🥳", "😏"
-                  ,"🚗", "🚕", "🚙", "🚌", "🚎", "🏎", "🚓", "🚑", "🚒", "🚐", "🛻"
-                  ,"🐶", "🐱", "🐭", "🐹", "🐰", "🦊", "🐻", "🐼", "🐻‍❄️", "🐨"]
+//    static var emojis = ["🥶", "😈", "👺", "💩", "👾", "🤥", "😮‍💨", "🤮", "🤯", "😱", "😨", "🤩","🥳", "😏"
+//                  ,"🚗", "🚕", "🚙", "🚌", "🚎", "🏎", "🚓", "🚑", "🚒", "🚐", "🛻"
+//                  ,"🐶", "🐱", "🐭", "🐹", "🐰", "🦊", "🐻", "🐼", "🐻‍❄️", "🐨"]
+    enum themes: CaseIterable{
+        case faces
+        case cars
+        case animals
+        case fruit
+    }
+
+    static var emojiMap = [
+        EmojiMemoryGame.themes.faces: ["🥶", "😈", "👺", "💩", "👾", "🤥", "😮‍💨", "🤮", "🤯", "😱", "😨", "🤩","🥳", "😏"],
+        EmojiMemoryGame.themes.cars: ["🚗", "🚕", "🚙", "🚌", "🚎", "🏎", "🚓", "🚑", "🚒", "🚐", "🛻"],
+        EmojiMemoryGame.themes.animals: ["🐶", "🐱", "🐭", "🐹", "🐰", "🦊", "🐻", "🐼", "🐻‍❄️", "🐨"],
+        EmojiMemoryGame.themes.fruit: ["🍏", "🍎", "🍐", "🍊", "🍋", "🍌", "🍉", "🍇", "🍓", "🫐"]
+    ]
 
     static func createMemoryGame() -> MemoryGame<String>{
-        MemoryGame<String>(numberOfPairsOfCards: Int.random(in: emojis.count/4..<emojis.count/2), createCardContent: { index in
-            emojis[index]
-        })
+        if var array = EmojiMemoryGame.themes.allCases.randomElement() {
+            var emojis = emojiMap[array]!
+            emojis.shuffle()
+            return MemoryGame<String>(numberOfPairsOfCards: Int.random(in: emojis.count / 2..<emojis.count), createCardContent: { index in
+                emojis[index]
+            })
+        } else {
+            var emojis = ["🥶", "😈", "👺", "💩", "👾", "🤥", "😮‍💨", "🤮", "🤯", "😱", "😨", "🤩","🥳", "😏"]
+            emojis.shuffle()
+            return MemoryGame<String>(numberOfPairsOfCards: Int.random(in: emojis.count / 4..<emojis.count / 2), createCardContent: { index in
+                emojis[index]
+            })
+        }
     }
 
     @Published private var model: MemoryGame<String> = createMemoryGame()
